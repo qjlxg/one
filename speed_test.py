1f019ea2316def5f84b2aeb690c1973325275c7a
qjlxg <qjlxg@163.com>
2026-02-06 15:50:51 +0800

    Update speed_test.py

--- speed_test.py
+++ speed_test.py
@@ -148,7 +148,7 @@ def run_speed_test():
         }, f)
     
     proc = subprocess.Popen(["./mihomo", "-f", "config.yaml"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    time.sleep(88)
+    time.sleep(188)
 
     valid_results = []
     country_counter = {}
